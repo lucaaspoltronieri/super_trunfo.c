37edd22b63d02c9395d60649c09b646cf097bf9e
Lucas P. <lucasgpoltronieri@gmail.com>
2025-07-18 11:09:42 -0300

    Criando variáveis

---
+++ Super Trunfo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    /*CARTA 01*/
+
+    char estado1; /*c*/
+    char codigo1; /*s*/
+    char cidade1[30]; /*s*/
+    int populacao1; /*f*/
+    float area1; /*e*/
+    float pib1; /*e*/
+    int pontoturistico1; /*d*/
+
+    /*CARTA 02*/
+
+    char estado2; /*c*/
+    char codigo2; /*s*/
+    char cidade2[30]; /*s*/
+    int populacao2; /*f*/
+    float area2; /*e*/
+    float pib2; /*e*/
+    int pontoturistico2; /*d*/
+
+}
